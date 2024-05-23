1a6b1e48bb827b61f948608300b712db3aa1ffeb
estebanbarthod <119411878+Estebanbrrr@users.noreply.github.com>
2024-05-23 17:19:54 +0200

    Update HoleDecider.java

--- HoleConsole/src/control/HoleDecider.java
+++ HoleConsole/src/control/HoleDecider.java
@@ -7,13 +7,11 @@ import boardifier.model.GameElement;
 import boardifier.model.Model;
 import boardifier.model.action.ActionList;
 import model.HoleBoard;
-import model.HolePawnPot;
 import model.HoleStageModel;
 import model.Pawn;
 
-import java.awt.*;
+import java.awt.Point;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Random;
 
 public class HoleDecider extends Decider {
@@ -26,58 +24,42 @@ public class HoleDecider extends Decider {
 
     @Override
     public ActionList decide() {
-        // do a cast get a variable of the real type to get access to the attributes of HoleStageModel
-        HoleStageModel stage = (HoleStageModel)model.getGameStage();
-        HoleBoard board = stage.getBoard(); // get the board
-        HolePawnPot pot = null; // the pot where to take a pawn
-        GameElement pawn = null; // the pawn that is moved
-        int rowDest = 0; // the dest. row in board
-        int colDest = 0; // the dest. col in board
+        HoleStageModel stage = (HoleStageModel) model.getGameStage();
+        HoleBoard board = stage.getBoard();
+        GameElement pawn = null;
+        int rowDest = 0;
+        int colDest = 0;
 
-        System.out.println("Deciding move for player: " + model.getIdPlayer());
+        HoleController holeController = (HoleController) control;
+        int pawnIndex = holeController.getPawnIndex();
 
-        if (model.getIdPlayer() == Pawn.PAWN_BLACK) {
-            pot = stage.getBlackPot();
-        } else {
-            pot = stage.getRedPot();
-        }
+        while (true) {
+            rowDest = loto.nextInt(7);
+            colDest = loto.nextInt(7);
 
-        if (pot == null) {
-            System.err.println("Error: Pot is null for player: " + model.getIdPlayer());
-        } else {
-            System.out.println("Using pot: " + (model.getIdPlayer() == Pawn.PAWN_BLACK ? "Black" : "Red"));
-        }
+            if (!board.isEmptyAt(rowDest, colDest)) continue;
 
-        for (int row = 0; row < 7; row++) {
-            for (int col = 0; col < 7; col++) {
-                Pawn p = (Pawn) pot.getElement(row, col);
-                if (p != null) {
-                    System.out.println("Found pawn at position: (" + row + ", " + col + ")");
-                    List<Point> valid = board.computeValidCells(p.getNumber());
-                    System.out.println("Valid cells for pawn " + p.getNumber() + ": " + valid);
-
-                    if (valid.size() != 0) {
-                        int id = loto.nextInt(valid.size());
-                        pawn = p;
-                        rowDest = valid.get(id).y;
-                        colDest = valid.get(id).x;
-                        System.out.println("Chosen cell for pawn " + p.getNumber() + ": (" + rowDest + ", " + colDest + ")");
-                        break;
-                    }
-                } else {
-                    System.out.println("No pawn at position: (" + row + ", " + col + ")");
-                }
+            if (pawnIndex >= 48) {
+                System.out.println("Tous les pions ont été placés.");
+                return new ActionList();
             }
+
+            if (model.getIdPlayer() == 0) {
+                pawn = stage.getBlackPawns()[pawnIndex];
+            } else {
+                pawn = stage.getRedPawns()[pawnIndex];
+            }
+
+            board.setValidCells(pawnIndex + 1);
+            if (board.canReachCell(rowDest, colDest)) break;
         }
 
-        if (pawn == null) {
-            System.err.println("Error: No pawn found to move for player: " + model.getIdPlayer());
-        }
+        holeController.incrementPawnIndex();
 
         ActionList actions = ActionFactory.generatePutInContainer(model, pawn, "holeboard", rowDest, colDest);
-        actions.setDoEndOfTurn(true); // after playing this action list, it will be the end of turn for current player.
+        actions.setDoEndOfTurn(true);
 
-        System.out.println("Generated actions: " + actions);
+        HoleBoard.lastCubePosition = new Point(colDest, rowDest);
 
         return actions;
     }
